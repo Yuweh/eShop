f7ce0326fd925317ddce6706c1e553a3a1be55e3
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-03-23 10:14:24 +0800

    eShop admin added

---
+++ eShopAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eShopAdmin
+//
+//  Created by Francis Jemuel Bergonia on 3/23/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
